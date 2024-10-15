1c9e1bd809e95eebea3b124fd8e941086d107214
AlvaroUrdayMunoz <Alvaro.urday@runa-it.com>
2024-10-15 02:15:03 -0500

    2024/10/15 2:14

--- ng-runa-v2/src/app/services/user.service.ts
+++ ng-runa-v2/src/app/services/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInAnonymously, signOut } from '@angular/fire/auth';
+import { Storage, ref, uploadBytes, listAll } from '@angular/fire/storage';
 import firebase from 'firebase/app';
 
 @Injectable({
@@ -7,21 +8,40 @@ import firebase from 'firebase/app';
 })
 export class UserService {
 
-  constructor(private auth: Auth) { }
+  constructor(private auth: Auth, private storage: Storage) { }
 
-  register({email, password}: any) {
-    return createUserWithEmailAndPassword(this.auth, email, password);
+  register = ({email, password}: any) => {
+    return createUserWithEmailAndPassword(this.auth, email, password)
   }
 
-  login({email, password}: any) {
-    return signInWithEmailAndPassword(this.auth, email, password);
+  login = ({email, password}: any) => {
+    return signInWithEmailAndPassword(this.auth, email, password)
   }
 
-  loginAnonymous() {
-    return signInAnonymously(this.auth);
+  loginAnonymous = () => {
+    return signInAnonymously(this.auth)
   }
 
-  async logout() {
-    return signOut(this.auth);
+  logout = () => {
+    return signOut(this.auth)
   }
+
+  uploadFile = (file : File) => {
+    const userRoute = localStorage.getItem("nombreUsuario")
+    const fileRef = ref(this.storage, `${userRoute}/${file.name}`)
+    return uploadBytes(fileRef, file)
+  }
+
+  getFiles = () => {
+    const userRoute = localStorage.getItem("nombreUsuario")
+    const fileRef = ref(this.storage,`${userRoute}`);
+    return listAll(fileRef)
+  }
+  /*
+  deleteFile = () => {
+    const userRoute = localStorage.getItem("nombreUsuario")
+    const fileRef = ref(this.storage,`${userRoute}`);
+    return fileRef.
+  }
+    */
 }
